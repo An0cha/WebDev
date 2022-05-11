afe9e26da80bb271ae35ce1e6c4381208975fd2d
Anocha <arnokung@gmail.com>
2022-05-11 15:50:31 -0400

    update css and about.html

--- css/myscript.js
+++ css/myscript.js
@@ -35,3 +35,20 @@ function scrollAnchors(e, respond = null) {
       }
   }, 100);
 }
+/*Show more less */
+function moreLessFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
