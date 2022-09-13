06e8ec7c4d163a26195825621d8109a09712abb0
Anocha <arnokung@gmail.com>
2022-09-13 10:36:00 -0400

    updated resume and html file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,10 @@
 resource "aws_s3_bucket" "mainbucket" {
     bucket = "rienit.com"
-    
+
+    website {
+        redirect_all_requests_to = "https://www.rienit.com"  
+    }
+
     tags = {
       name = "Zone Apex Bucket"
     }
